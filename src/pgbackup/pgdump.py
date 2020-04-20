a9ea081f2b2b79493f16411ea2af9c3696b26725
ann <anfimki@mail.ru>
2020-04-20 13:28:47 +0000

    updated cli and setup

--- src/pgbackup/pgdump.py
+++ src/pgbackup/pgdump.py
@@ -6,3 +6,12 @@ def dump(url):
      except 0SError as err:
          print(f"Error: {err}")
          sys.exit(1)
+
+
+def dump_file_name(url, timestamp=None):
+        db_name = url.split("/")[-1]
+        db_name = db_name.split("?")[0]
+        if timestamp:
+            return f"{db_name}-{timestamp}.sql"
+        else:
+            return f"{db_name}.sql"
